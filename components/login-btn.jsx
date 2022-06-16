1d3b33da22e0f3f8918ff8c5fab15e40a24c5db6
Panupong Chamsomboon <5635512110@psu.ac.th>
2022-06-16 03:59:43 +0000

    [ADD next-auth

---
+++ components/login-btn.jsx
@@ -0,0 +1,21 @@
+import { useSession, signIn, signOut } from "next-auth/react"
+export default function Component() {
+  const { data: session } = useSession()
+  const fullData = useSession()
+  console.log('session', session)
+  console.log('fullData', fullData)
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    )
+  }
+  return (
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
+  )
+}
